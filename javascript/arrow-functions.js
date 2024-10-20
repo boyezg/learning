09a9ba553efd7b592a8420d5d43c67eedcbde73e
Boyez <boyez.gollapalli@ensarsolutions.com>
2024-10-20 23:12:11 +0530

    arrow functions

---
+++ javascript/arrow-functions.js
@@ -0,0 +1,26 @@
+//Arrow functions are writing many types those are
+
+//type 1 parameter passing with some function actions logic
+
+// const addNumbers = (a,b) => {
+//     return a+b;
+// }
+
+// let addNumbersValue = addNumbers(100, 200);
+// console.log(addNumbersValue)
+
+///////////////////////
+
+//type 2 for single or more parameter passing
+
+// const showNumber = (a) => a;
+// let showNumberValue = showNumber(2000);
+// console.log(showNumberValue)
+
+//////////////////////
+
+//type 3 for single parameter passing
+
+// const showNum = a => a;
+// let showNumValue = showNum(2000);
+// // console.log(showNum)
